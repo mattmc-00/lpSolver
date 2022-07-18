67d6bac1fc3bd5b113061c5241360b4a347974b8
matth <matthewmclean@uvic.ca>
2022-07-17 22:19:49 -0700

    Seems to be fully functional, uses bland's rule and the primal-dual method.

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def i_matrix_list(n):
 
 
 def gauss(m_a, v_a):
-    # Concatenate m_a and v_a
     aug_a = np.hstack((m_a, v_a))
 
     a_rows = len(aug_a[:, 0])
@@ -81,69 +80,74 @@ def primal_simplex(a, b, c, B, N):
     }
     n_plus_m = len(N) + len(B)
     b_vect = reshape_v(b)
+    c_vect = reshape_v(c)
 
     x = [fr.Fraction(0, 1)] * n_plus_m
     x_b = gauss(a[:, B], b_vect)
-    for i in range(len(B)):
-        x[B[i]] = x_b[i]
-    for i in B:
-        if x[i] < 0:
+    for k in x_b:
+        if k < 0:
             return_dict["result"] = "infeasible"
             return return_dict
 
-    # start loop
+    for k in range(len(B)):
+        x[B[k]] = x_b[k]
+
     while True:
-        z = [fr.Fraction(0, 1)] * n_plus_m
-        # z_b = [fr.Fraction(0, 1)] * len(N)
-        c_vect = reshape_v(c)
         v = gauss(np.transpose(a[:, B]), c_vect[B])
-        z_n = np.transpose(a[:, N]).dot(v) - c[N]
-        for i in range(len(N)):
-            z[N[i]] = z_n[i]
-        # check if optimal, not done
-        for i in N:
-            if z[i] < 0:
+        v = reshape_v(v)
+
+        z = [fr.Fraction(0, 1)] * n_plus_m
+        z_n = (np.transpose(a[:, N])).dot(v) - c_vect[N]
+        for k in range(len(N)):
+            z[N[k]] = z_n[k]
+
+        # Check if optimal
+        for k in N:
+            if z[k] < 0:
+                # pivot rule: bland
+                j = k
                 break
-            if i == N[-1]:
+            elif k == N[-1]:
                 return_dict["result"] = "optimal"
                 return_dict["solutions"] = x
+                return_dict["basis"] = B
+                return_dict["non-basis"] = N
                 return return_dict
-        # pivot rule: bland
-        for i in N:
-            if z[i] < 0:
-                j = i
-                break
 
-        a_j = np.array(a[:, j])
-        a_j = reshape_v(a_j)
+        a_j = reshape_v(np.array(a[:, j]))
+
         delta_x_b = gauss(a[:, B], a_j)
         delta_x = [fr.Fraction(0, 1)] * n_plus_m
-        for i in range(len(B)):
-            delta_x[B[i]] = delta_x_b[i]
+        for k in range(len(B)):
+            delta_x[B[k]] = delta_x_b[k]
 
         t_list = []
-        for i in B:
-            if delta_x[i] > 0:
-                t_list.append(x[i] / delta_x[i])
+        for k in B:
+            if delta_x[k] > 0:
+                t_list.append(x[k] / delta_x[k])
+
         if len(t_list) == 0:
             return_dict["result"] = "unbounded"
             return return_dict
 
         t = min(t_list)
+
         # bland again
         for k in B:
             if delta_x[k] > 0:
                 if (x[k] / delta_x[k]) == t:
                     i = k
+                    break
 
         for k in B:
-            x[k] = x[k] - t * delta_x[k]
+            x[k] = x[k] - (t * delta_x[k])
 
         x[j] = t
         bi.insort(B, j)
         B.remove(i)
         bi.insort(N, i)
         N.remove(j)
+        print(B)
 
 
 def dual_simplex(a, b, c, B, N):
@@ -152,70 +156,124 @@ def dual_simplex(a, b, c, B, N):
     }
     n_plus_m = len(N) + len(B)
     b_vect = reshape_v(b)
-
-    z = [fr.Fraction(0, 1)] * n_plus_m
     c_vect = reshape_v(c)
+
     v = gauss(np.transpose(a[:, B]), c_vect[B])
-    z_n = np.transpose(a[:, N]).dot(v) - c[N]
+    v = reshape_v(v)
+    z = [fr.Fraction(0, 1)] * n_plus_m
+    z_n = (np.transpose(a[:, N])).dot(v) - c_vect[N]
+
+    for k in z_n:
+        if k < 0:
+            return_dict["result"] = "infeasible"
+            return return_dict
+
     for k in range(len(N)):
         z[N[k]] = z_n[k]
 
-    if min(z) < 0:
-        return_dict["result"] = "infeasible"
+    while True:
+        x = [fr.Fraction(0, 1)] * n_plus_m
+        x_b = gauss(a[:, B], b_vect)
+        for k in range(len(B)):
+            x[B[k]] = x_b[k]
 
-    # start loop
-    x = [fr.Fraction(0, 1)] * n_plus_m
-    x_b = gauss(a[:, B], b_vect)
-    for k in range(len(B)):
-        x[B[k]] = x_b[k]
+        for k in B:
+            if x[k] < 0:
+                # pivot rule: bland
+                i = k
+                break
+            if k == B[-1]:
+                return_dict["result"] = "optimal"
+                return_dict["solutions"] = x
+                return_dict["basis"] = B
+                return_dict["non-basis"] = N
+                return return_dict
 
-    for k in B:
-        if x[k] < 0:
-            break
-        if k == B[-1]:
-            return_dict["result"] = "optimal"
-            # I have no idea what the return value is, maybe z
-            return_dict["solutions"] = z
+        # x_b_vect = reshape_v(x_b)
+        # i = x.index(min(x_b_vect))
+
+        u_list = [fr.Fraction(0, 1)] * len(B)
+        u_list[B.index(i)] = fr.Fraction(1, 1)
+        u = reshape_v(np.array(u_list))
+
+        v = gauss(np.transpose(a[:, B]), u)
+        v = reshape_v(v)
+
+        delta_z = [fr.Fraction(0, 1)] * n_plus_m
+        delta_z_n = (-1 * np.transpose(a[:, N])).dot(v)
+        for k in range(len(N)):
+            delta_z[N[k]] = delta_z_n[k]
+
+        s_list = []
+        for j in N:
+            if delta_z[j] > 0:
+                s_list.append(z[j] / delta_z[j])
+
+        if len(s_list) == 0:
+            # d unbounded, implies the primal problem is infeasible
+            return_dict["result"] = "infeasible"
             return return_dict
 
-    # pivot rule: bland
-    for k in B:
-        if x[k] < 0:
-            i = k
-            break
-    u_list = [fr.Fraction(0, 1)] * len(B)
-    u_list[B.index(i)] = fr.Fraction(1, 1)
+        s = min(s_list)
 
-    u = reshape_v(np.array(u_list))
-    delta_z = [fr.Fraction(0, 1)] * n_plus_m
-    v = gauss(np.transpose(a[:, B]), u)
-    delta_z_n = (-1 * np.transpose(a[:, N])).dot(v)
-    for k in range(len(N)):
-        delta_z[N[k]] = delta_z_n[k]
+        # bland again
+        for k in N:
+            if delta_z[k] > 0:
+                if (z[k] / delta_z[k]) == s:
+                    j = k
+                    break
 
-    s_list = []
-    for j in N:
-        if delta_z[j] > 0:
-            s_list.append(z[j] / delta_z[j])
-    if len(s_list) == 0:
-        # d unbounded, implies the primal problem is infeasible
-        return_dict["result"] = "unbounded"
-        return return_dict
+        for k in N:
+            z[k] = z[k] - s * delta_z[k]
 
-    s = min(s_list)
-    for k in N:
-        if delta_z[k] > 0:
-            if (z[k] / delta_z[k]) == s:
-                j = k
+        z[i] = s
+        bi.insort(B, j)
+        B.remove(i)
+        bi.insort(N, i)
+        N.remove(j)
+        print(B)
 
 
 def main():
-    # file_path = "test_LPs_volume1/input/445k22_A1_juice.txt"
-    # file_path = "test_LPs_volume1/input/445k22_Lecture01_bakery.txt"
-    # file_path = "test_LPs_volume1/input/netlib_adlittle.txt"
-    # file_path = "test_LPs_volume1/input/netlib_afiro.txt"
-    # file_path = "test_LPs_volume1/input/netlib_bgprtr.txt"
-    file_path = "test_LPs_volume1/input/netlib_itest2.txt"
+    files = [
+        "test_LPs_volume1/input/445k22_A1_juice.txt",
+        "test_LPs_volume1/input/445k22_Lecture01_bakery.txt",
+        # "test_LPs_volume1/input/netlib_adlittle.txt",
+        # "test_LPs_volume1/input/netlib_afiro.txt",
+        # "test_LPs_volume1/input/netlib_bgprtr.txt",
+        # "test_LPs_volume1/input/netlib_itest2.txt",
+        # "test_LPs_volume1/input/netlib_itest6.txt",
+        # "test_LPs_volume1/input/netlib_klein1.txt",
+        # "test_LPs_volume1/input/netlib_klein2.txt",
+        # "test_LPs_volume1/input/netlib_sc50a.txt",
+        # "test_LPs_volume1/input/netlib_sc50b.txt",
+        # "test_LPs_volume1/input/netlib_sc105.txt",
+        # "test_LPs_volume1/input/netlib_scagr7.txt",
+        # "test_LPs_volume1/input/netlib_share1b.txt",
+        # "test_LPs_volume1/input/netlib_share2b.txt",
+        # "test_LPs_volume1/input/netlib_stocfor1.txt",
+        "test_LPs_volume1/input/vanderbei_example2.1.txt",
+        "test_LPs_volume1/input/vanderbei_example3.6.txt",
+        "test_LPs_volume1/input/vanderbei_example5.6.txt",
+        "test_LPs_volume1/input/vanderbei_example6.3.txt",
+        "test_LPs_volume1/input/vanderbei_example14.1.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.1.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.2.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.3.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.4.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.5.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.6.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.7.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.8.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.9.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.10.txt",
+        "test_LPs_volume1/input/vanderbei_exercise2.11.txt",
+    ]
+    for file_name in files:
+        simplex_solver(file_name)
+
+
+def simplex_solver(file_path):
     with open(file_path) as input_file:
         input_data = input_file.read()
     input_list = []
@@ -254,18 +312,36 @@ def main():
             B.append(i)
 
     if min(b) >= 0:
+        # Primal feasible
+        print("primal")
         sol = primal_simplex(a, b, c, B, N)
     elif max(c) <= 0:
-        print("dual time")
-        dual_simplex(a, b, c, B, N)
+        # Dual feasible
+        print("dual")
+        sol = dual_simplex(a, b, c, B, N)
     else:
-        print("not ready")
+        # Neither primal nor dual feasible
+        print("neither")
+        zeros_list = [fr.Fraction(0, 1)] * len(c)
+        zeros = reshape_v(np.array(zeros_list))
+        aux_sol = dual_simplex(a, b, zeros, B, N)
+        if aux_sol["result"] == "infeasible":
+            print(aux_sol["result"])
+            return 0
+        elif aux_sol["result"] == "unbounded":
+            print(aux_sol["result"])
+            return 0
+        elif aux_sol["result"] == "optimal":
+            B_prime = aux_sol["basis"]
+            N_prime = aux_sol["non-basis"]
+            sol = primal_simplex(a, b, c, B_prime, N_prime)
 
-    return 0
     if sol["result"] == "infeasible":
         print(sol["result"])
+        return 0
     elif sol["result"] == "unbounded":
         print(sol["result"])
+        return 0
     elif sol["result"] == "optimal":
         print(sol["result"])
         solutions = []
@@ -283,6 +359,7 @@ def main():
         formatted_optimum = f'{float_optimum:.7}'
         print(formatted_optimum)
         print(" ".join(solutions))
+    return 0
 
 
 if __name__ == '__main__':
